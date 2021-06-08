a0bbba90df16855e4ce9d186b0ad24b15db1da29
Thomas Dacey <DaceyTom2@gmail.com>
2021-06-08 14:04:22 -0400

    added messages container

---
+++ src/components/organisms/MessagesContainer.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex">
+    <div class="w-full md:w-1/4 md:border md:border-r-0 md:border-gray">Inbox</div>
+    <div class="flex-auto hidden md:block md:border md:border-gray">Conversational Area</div>
+  </div>
+</template>
+
+<script lang="ts">
+export default ({
+
+  setup() {
+    return {
+    };
+  },
+
+});
+</script>
