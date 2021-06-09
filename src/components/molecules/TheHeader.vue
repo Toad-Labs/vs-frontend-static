55048f728c4ab4b970c1870f9b881319ffc0d9af
shewood <shewood01@gmail.com>
2021-06-09 09:51:02 -0400

    added line to header

--- src/components/molecules/TheHeader.vue
+++ src/components/molecules/TheHeader.vue
@@ -1,47 +1,67 @@
 <template>
   <div>
     <header>
-      <div class="bg-gray-200">
-         <!-- Hidden heading -->
-          <h2 class="sr-only">Site Header Information</h2>
-                  <div
-          class="w-full mx-auto  p-5 flex items-center justify-end bg-gray-default"
-        >
-
-        </div>
+      <div class="pb-2 sm:pb-3 border-b-2 border-gray-200">
+        <!-- Hidden heading -->
+        <h2 class="sr-only">Site Header Information</h2>
         <div
-          class="sm:flex hidden w-full mx-auto container px-1 items-center justify-end bg-pink-300"
+          class="
+            w-full
+            mx-auto
+            p-5
+            flex
+            items-center
+            justify-end
+            bg-gray-default
+          "
+        ></div>
+        <div
+          class="
+            sm:flex
+            hidden
+            w-full
+            mx-auto
+            container
+            px-1
+            items-center
+            justify-end
+          "
         >
-
           <!-- Language toggle on desktop -->
-         
-              <a class="  bg-blue-300 m-1 font-body underline">
-              Français
-               <!-- {language === "en" ? "English" : "Français"} -->
-              </a>
-         
-            <!-- end lang toggle desktop -->
-        </div>
-       
 
-      
+          <a class="m-1 font-body underline">
+            Français
+            <!-- {language === "en" ? "English" : "Français"} -->
+          </a>
+
+          <!-- end lang toggle desktop -->
+        </div>
 
         <!-- start gc logo -->
-         <!-- src={language === "en" ? "../../assets/sig-blk-en.svg" : "../../assets/sig-blk-fr.svg"} -->
-        <div className="flex flex-col sm:flex-row container w-full mx-auto p-3 justify-between items-center bg-yellow-200 ">
-          
-          <div className="flex flex-row items-center w-full justify-between bg-green-200">
+        <!-- src={language === "en" ? "../../assets/sig-blk-en.svg" : "../../assets/sig-blk-fr.svg"} -->
+        <div
+          className="flex flex-col sm:flex-row container w-full mx-auto p-1 justify-between items-center"
+        >
+          <div className="flex flex-row items-center w-full justify-between">
             <img
               className="h-6  sm:h-8 md:h-8 lg:h-7 xl:h-8  "
               src="../../assets/sig-blk-en.svg"
               alt="canada logo en"
             />
-          <a class="sm:hidden  bg-blue-300 m-3 sm:m-0 font-semibold font-body underline">
+            <a
+              class="
+                sm:hidden
+                bg-blue-300
+                m-3
+                sm:m-0
+                font-semibold font-body
+                underline
+              "
+            >
               FR
-               <!-- {language === "en" ? "EN" : "FR"} -->
-              </a>
+              <!-- {language === "en" ? "EN" : "FR"} -->
+            </a>
           </div>
-            
           <!-- end gc logo -->
           <!-- start gc search bar -->
           <div className="inline-flex w-full sm:w-80 sm:h-10">
@@ -59,19 +79,15 @@
                 type="submit"
                 class="
                   bg-primary-main
-                  text-white 
-                  text-center 
+                  text-white text-center
                   p-2
                   rounded-none
-                 
                   hover:bg-gray-default
                   active:bg-gray-default
                   focus:bg-gray-default
                 "
               >
-                <span
-                  >  
-                  
+                <span>
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     class="h-6 w-6"
