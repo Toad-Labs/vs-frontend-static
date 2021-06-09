2c67ce750c483304d5483a8da75e40e69a7c22ab
Thomas Dacey <DaceyTom2@gmail.com>
2021-06-09 09:57:00 -0400

    add inbox to message container

--- src/components/organisms/MessagesContainer.vue
+++ src/components/organisms/MessagesContainer.vue
@@ -1,17 +1,22 @@
 <template>
   <div class="flex">
-    <div class="w-full md:w-1/4 md:border md:border-r-0 md:border-gray">Inbox</div>
+    <div class="w-full md:w-1/3 md:border md:border-r-0 md:border-gray">
+      <inbox />
+    </div>
     <div class="flex-auto hidden md:block md:border md:border-gray">Conversational Area</div>
   </div>
 </template>
 
 <script lang="ts">
+import Inbox from '../molecules/Inbox.vue';
 export default ({
 
   setup() {
     return {
     };
   },
-
+  components: {
+    Inbox
+  },
 });
 </script>
