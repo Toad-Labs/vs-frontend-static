50e873935ee46a94435d3c09cb233dffb7a16a6a
Tisham Islam <45071113+TishamIslam@users.noreply.github.com>
2021-06-09 09:04:44 -0400

    Removed report button to follow figma; finished

--- src/components/molecules/ButtonFooter.vue
+++ src/components/molecules/ButtonFooter.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="flex flex-col w-full items-center m-0 md:mx-auto md:block">
+    <div class="flex flex-col w-full items-center container mx-auto md:p-5 md:block">
         <long-button colorClass="bg-btn-dark text-gray-light" text="Provide Feedback" />
-        <long-button colorClass="bg-btn-light text-primary-main" text="Report a problem or a mistake on this page" />
+        <!-- <long-button colorClass="bg-btn-light text-primary-main" text="Report a problem or a mistake on this page" /> -->
     </div>
 </template>
 
