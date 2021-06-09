52266716d1f2a574539d7233aadbd69bb2e8cbb1
Thomas Dacey <DaceyTom2@gmail.com>
2021-06-09 08:52:56 -0400

    Added inbox container

---
+++ src/components/molecules/Inbox.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1 class="text-4xl">Inbox</h1>
+  <inbox-item />
+  <inbox-item dayRead="Weds" selected='true' />
+  <inbox-item dayRead="Weds" />
+</template>
+
+<script lang="ts">
+import InboxItem from './InboxItem.vue';
+export default ({
+
+  setup() {
+    return {
+    };
+  },
+  components: {
+    InboxItem
+  },
+});
+</script>
