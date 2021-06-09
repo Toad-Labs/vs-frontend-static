f77bb4e54a3f5ffc8c90f208534c3a76d31aaf41
Thomas Dacey <DaceyTom2@gmail.com>
2021-06-09 08:52:43 -0400

    Added inbox item

---
+++ src/components/atoms/ReadNotification.vue
@@ -0,0 +1,23 @@
+<template>
+
+  
+  <div v-if="dayRead" class="flex items-center">
+    <p>{{dayRead}}</p>
+  </div>
+  <div v-else class="flex items-center justify-center h-full">
+    <div class="bg-primary-light rounded-full w-4 h-4" />
+  </div>
+</template>
+
+<script lang="ts">
+export default ({
+  props: {
+    dayRead: String
+  },
+  setup() {
+    return {
+    };
+  },
+
+});
+</script>
---
+++ src/components/molecules/InboxItem.vue
@@ -0,0 +1,33 @@
+<template>
+  <div :class="[selected ? 'bg-primary-light':'', 'flex items-center w-full h-20']">
+    <div class="p-4">
+      <div class="bg-primary-light rounded-full w-12 h-12 flex items-center justify-center">
+        J
+      </div>
+    </div>
+    <div class="flex-auto">
+      <p>Virtual Concierge</p>
+      <p>Hi Mary, I'm your Virtual Con...</p>
+    </div>
+    <div class="p-4 w-20 h-20">
+      <read-notification v-bind:dayRead='dayRead' />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import ReadNotification from '../atoms/ReadNotification.vue';
+export default ({
+  props: {
+    dayRead: String,
+    selected: Boolean
+  },
+  setup() {
+    return {
+    };
+  },
+  components: {
+    ReadNotification
+  },
+});
+</script>
