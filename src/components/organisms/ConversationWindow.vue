314d53dc69b67892fa64915a8f9c88be22f74bbf
Thomas Dacey <DaceyTom2@gmail.com>
2021-06-10 10:52:26 -0400

    Cleaned up scrolling and drawer issue

--- src/components/organisms/ConversationWindow.vue
+++ src/components/organisms/ConversationWindow.vue
@@ -1,12 +1,12 @@
 <template>
-    <div class="relative w-full">
+    <div class="w-full h-full flex flex-col">
         <div class="flex border-b border-gray-200 p-2">
             <img src="../../assets/logo.png" alt="Bot Image." class="h-10 mt-auto w-10">
             <span class=" font-bold pt-2 pl-2">Jerry S. (Do Not Reply)</span>
         </div> 
-        <div class="flex">
+        <div class="flex h-full overflow-auto">
             <img src="../../assets/logo.png" alt="Bot Image." class="h-10 mt-auto w-10">
-            <div class="w-full pr-6 overflow-auto max-h-96 flex flex-col-reverse">
+            <div class="w-full pr-6 overflow-auto flex flex-col-reverse">
                 <!-- New text dialogs need to be added at the top since the div is flex reverse -->
                 <ConversationMessage :isUser=false :text="'Oh... Well, this is akward.'" />
                 <ConversationMessage :isUser=true :text="'I meant I am working too hard; I am all fired up right now.'"/>
@@ -24,7 +24,7 @@
                 <p class="text-center font-light text-gray-400">WEDS 10:04 AM </p>
             </div>     
         </div>
-            <TextInput />
+        <TextInput />
     </div>
 </template>
 <script>
--- src/components/organisms/MessagesContainer.vue
+++ src/components/organisms/MessagesContainer.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="flex">
-    <div class="w-full md:w-1/3 md:border md:border-r-0 md:border-gray">
-      <inbox />
+    <div class="w-full md:w-1/3 md:border md:border-r-0 md:border-gray md:h-96">
+      <div class="h-full md:overflow-auto">
+        <inbox />
+      </div>
     </div>
-    <div class="flex-auto hidden md:block md:border md:border-gray">
+    <div :class='[drawerOpen? "": "hidden", "bg-white w-screen h-screen absolute md:relative top-0 left-0 md:flex-auto md:block md:border md:border-gray md:h-96"]'>
       <ConversationWindow />
     </div>
   </div>
@@ -15,6 +17,9 @@ import Inbox from '../molecules/Inbox.vue';
 import ConversationWindow from "./ConversationWindow.vue";
 
 export default ({
+  props: {  
+    drawerOpen: Boolean,
+  },
   components: {
     ConversationWindow,
     Inbox,
