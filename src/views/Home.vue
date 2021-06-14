d65e91d09e639113d3803995061ac9e6ffc8149b
Tisham Islam <45071113+TishamIslam@users.noreply.github.com>
2021-06-14 13:46:38 -0400

    forgot to remove import + change colour back

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,14 +6,12 @@
 
 <script>
 import MessagesContainer from '../components/organisms/MessagesContainer.vue';
-import MailContainer from '../components/organisms/MailContainer.vue';
 // @ is an alias to /src
 
 export default {
   name: "Home",
   components: {
     MailContainer,
-    MessagesContainer
   },
 };
 </script>
