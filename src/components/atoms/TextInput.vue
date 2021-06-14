8deb633cb66ce658e1de127daf2a4ad4ccc98ec3
Benoit Cloutier <benoit.bc.cloutier@gmail.com>
2021-06-14 12:22:55 -0400

    Text input feedback.

--- src/components/atoms/TextInput.vue
+++ src/components/atoms/TextInput.vue
@@ -2,19 +2,18 @@
   <input
     type="text"
     placeholder="Write something..."
-    class="w-full border-t border-b border-gray-200 p-3 mb-px"
+    class="w-full border-t border-b border-gray-200 p-3"
   />
-  <div class="w-full bg-gray-infolt">
+  <div class="w-full bg-gray-infolt mt-px">
     <chat-option-button
       v-for="(option, index) in buttonOptions"
       :key="index"
       :text="option"
     />
-    <button class="float-right cursor-pointer my-2">
+    <button class="float-right cursor-pointer">
       <img
         src="../../assets/Send/Active/Default.svg"
         alt="Bot Image."
-        class="h-8"
       />
     </button>
   </div>
