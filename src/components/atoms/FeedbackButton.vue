e52f668c19ca9ec6a3b5e1c75eda8a6c5343163b
Tisham Islam <45071113+TishamIslam@users.noreply.github.com>
2021-06-14 10:32:21 -0400

    realized that I used a breakpoint to apply the same thing...

--- src/components/atoms/FeedbackButton.vue
+++ src/components/atoms/FeedbackButton.vue
@@ -1,5 +1,5 @@
 <template>    
-    <button :class="'w-full rounded justify-center py-2 mt-6 md:mt-6 md:w-1/3 ' + colorClass ">
+    <button :class="'w-full rounded justify-center py-2 mt-6 md:w-1/3 ' + colorClass ">
       {{ text }}
     </button>
 </template>
