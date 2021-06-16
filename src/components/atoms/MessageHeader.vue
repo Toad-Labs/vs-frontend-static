4efeda40c4cd8e8ee856f4d700f7907bbf030ae6
shewood <shewood01@gmail.com>
2021-06-16 19:21:27 -0400

    updated fonts as per figma

--- src/components/atoms/MessageHeader.vue
+++ src/components/atoms/MessageHeader.vue
@@ -1,25 +1,23 @@
 <template>
-    <div class="flex border-b border-gray-200 p-2">
-        <img :src="icons[imageName]" :alt="altText" class="h-10 mt-auto w-10">
-        <span class=" font-bold pt-2 pl-2">{{ headerText }}</span>
-    </div> 
+  <div class="flex border-b border-gray-200 p-2">
+    <img :src="icons[imageName]" :alt="altText" class="h-10 mt-auto w-10" />
+    <span class="font-bold font-body pt-2 pl-2">{{ headerText }}</span>
+  </div>
 </template>
 
 <script>
 import icons from "../../assets/UserAccount/icons.js";
 export default {
-    data () {
-        return {
-            icons
-        };
-    },
-    props: {
-        imageName: String,
-        altText: String,
-        headerText: String,
-    },
-    setup() {
-        
-    },
-}
+  data() {
+    return {
+      icons,
+    };
+  },
+  props: {
+    imageName: String,
+    altText: String,
+    headerText: String,
+  },
+  setup() {},
+};
 </script>
