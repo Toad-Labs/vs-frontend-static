e650d409e423a925a62920d9e3936d7c51e5fd24
shewood <shewood01@gmail.com>
2021-06-22 11:35:08 -0400

    reduced the hight of the banner

--- src/components/atoms/Banner.vue
+++ src/components/atoms/Banner.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-banner-img bg-cover py-2 sm:py-8 px-2 sm:p-10">
+  <div class="bg-banner-img bg-cover py-2 sm:py-5 2xl:py-6 px-2 sm:p-10">
     <div class="mx-auto container sm:px-3">
       <h1
         class="
