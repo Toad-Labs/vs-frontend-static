153b823d2f35bf53f890b8b46c7b5f0765b3cf17
Benoit Cloutier <benoit.bc.cloutier@gmail.com>
2021-06-22 13:04:49 -0400

    Added a little bit of aria labels.

--- src/components/atoms/MessageHeader.vue
+++ src/components/atoms/MessageHeader.vue
@@ -1,14 +1,19 @@
 <template>
   <div class="flex border-b border-gray-200 p-2">
-    <span>
-      <img
-        :src="icons[backIcon]"
-        :alt="altText"
-        class="sm:hidden h-10 mt-auto w-10 mr-4"
-        @click="clickBack"
-      />
-    </span>
-    <img :src="icons[imageName]" :alt="altText" class="h-10 mt-auto w-10" />
+    <img
+      :src="icons[backIcon]"
+      :alt="altText"
+      class="sm:hidden h-10 mt-auto w-10 mr-4"
+      @click="clickBack"
+      aria-label="close modal"
+      tabindex="0"
+    />
+    <img
+      :src="icons[imageName]"
+      :alt="altText"
+      class="h-10 mt-auto w-10"
+      aria-hidden="true"
+    />
     <span class="font-bold font-body pt-2 pl-2">{{ headerText }}</span>
   </div>
 </template>
