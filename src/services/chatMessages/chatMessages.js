7ca666e1539cfc88281a92734d1fc4fbc506af33
Benoit Cloutier <benoit.bc.cloutier@gmail.com>
2021-06-25 09:17:42 -0400

    Removed sample code.

--- src/services/chatMessages/chatMessages.js
+++ src/services/chatMessages/chatMessages.js
@@ -1,27 +1,4 @@
-import { DirectLine } from "botframework-directlinejs";
 import request from "../api";
-
-var directLine = new DirectLine({
-  secret:
-    "yUFfstW-qB4.-5pEc2KNRFxKZt4sbzLnvMAn4NWok31MfECCT6lMMX0" /* put your Direct Line secret here */,
-});
-
-console.log(directLine);
-
-directLine
-  .postActivity({
-    from: { id: "myUserId", name: "myUserName" }, // required (from.name is optional)
-    type: "message",
-    text: "a message for you, Rudy",
-  })
-  .subscribe(
-    (id) => console.log("Posted activity, assigned ID ", id),
-    (error) => console.log("Error posting activity", error)
-  );
-
-directLine.activity$.subscribe((activity) =>
-  console.log("received activity ", activity)
-);
 
 function getAll() {
   return request({
