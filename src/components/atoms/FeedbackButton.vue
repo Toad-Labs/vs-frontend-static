247eeec5f00e6e1d030e550dcbce1a0540fa7283
Tisham Islam <45071113+TishamIslam@users.noreply.github.com>
2021-06-28 12:51:40 -0400

    aligned feedback button to inbox on all sizes

--- src/components/atoms/FeedbackButton.vue
+++ src/components/atoms/FeedbackButton.vue
@@ -1,13 +1,15 @@
 <template>
-  <button
-    :class="
-      'w-full rounded justify-center py-2 mt-6 md:w-1/3 md:max-w-sm' +
-      ' ' +
-      colorClass
-    "
-  >
-    {{ text }}
-  </button>
+  <div class="px-1 xs:px-0 pt-6">
+    <button
+      :class="
+        'w-full rounded justify-center py-2 sm:w-2/5 md:w-1/3 md:max-w-sm' +
+        ' ' +
+        colorClass
+      "
+    >
+      {{ text }}
+    </button>
+  </div>
 </template>
 
 <script lang="ts">
