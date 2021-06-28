89fd1fd73dd7d2ec3a99d24f9437c56638658e66
Tisham Islam <45071113+TishamIslam@users.noreply.github.com>
2021-06-28 14:32:31 -0400

    split some containers apart

--- src/components/atoms/FeedbackButton.vue
+++ src/components/atoms/FeedbackButton.vue
@@ -1,14 +1,16 @@
 <template>
-  <div class="px-1 xs:px-0 pt-6">
-    <button
-      :class="
-        'w-full rounded justify-center py-2 sm:w-2/5 md:w-1/3 md:max-w-sm' +
-        ' ' +
-        colorClass
-      "
-    >
-      {{ text }}
-    </button>
+  <div class="px-2 sm:px-0">
+    <div class="mx-auto container pt-6">
+      <button
+        :class="
+          'w-full rounded justify-center py-2 sm:w-2/5 md:w-1/3 md:max-w-sm' +
+          ' ' +
+          colorClass
+        "
+      >
+        {{ text }}
+      </button>
+    </div>
   </div>
 </template>
 
