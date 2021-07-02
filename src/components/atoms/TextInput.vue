06ecee4ef6b309afee5fc4c09a838370bb4a9a00
Tisham Islam <45071113+TishamIslam@users.noreply.github.com>
2021-07-02 12:46:58 -0400

    focus includes hover + untabbable while inactive

--- src/components/atoms/TextInput.vue
+++ src/components/atoms/TextInput.vue
@@ -1,5 +1,6 @@
 <template>
   <input
+    ref="input"
     type="text"
     placeholder="Write something..."
     class="w-full border-t border-b border-gray-200 p-3 bg-clip-padding"
@@ -16,10 +17,14 @@
       @send-button="sendBtnText"
     />
     <button
+      ref="sendMsgBtn"
       class="float-right cursor-pointer"
       aria-label="Send message"
+      tabindex="0"
       @click="sendText"
       @focus="onSendBtnFocus"
+      @mouseover="onSendBtnFocus"
+      @mouseleave="checkSendBtnActive"
       @blur="checkSendBtnActive"
     >
       <img
@@ -52,11 +57,17 @@ export default {
     const text = ref("");
     const iconState = ref("SendInactive");
 
+    //these refs are reassigned to their respective components on mounting
+    //as with other refs, the actual elements are accessed with .value (input.value, for example)
+    const input = ref(null);
+    const sendMsgBtn = ref(null);
+
     function sendText() {
       if (this.text.length > 0) {
         context.emit("add-message", this.text);
         text.value = "";
         checkSendBtnActive();
+        input.value.focus();
       }
     }
     function sendBtnText(btnText) {
@@ -69,9 +80,13 @@ export default {
         text.value === "" ? "SendInactive" : "SendActiveFocused";
     }
     function checkSendBtnActive() {
-      iconState.value = text.value === "" ? "SendInactive" : "SendActive";
+      let textEmpty = text.value === "";
+      sendMsgBtn.value.tabIndex = textEmpty ? -1 : 0; //the send button will be untabbable if there's no text
+      iconState.value = textEmpty ? "SendInactive" : "SendActive";
     }
     return {
+      sendMsgBtn,
+      input,
       text,
       icons,
       iconState,
