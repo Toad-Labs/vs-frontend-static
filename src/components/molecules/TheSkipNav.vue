202e90b776f09cd9905126a0f87a1f3ada1cf73e
Tisham Islam <45071113+TishamIslam@users.noreply.github.com>
2021-07-05 15:20:02 -0400

    added skip nav functionality

--- src/components/molecules/TheSkipNav.vue
+++ src/components/molecules/TheSkipNav.vue
@@ -1,8 +1,14 @@
 <template>
   <nav>
     <!-- Site Accessibility Navigation -->
-    <ul class="sr-only" aria-label="Skip Navigation Link">
-      <li>
+    <ul
+      class="sr-only focus-within:not-sr-only text-center"
+      aria-label="Skip Navigation Link"
+    >
+      <li
+        class="w-max bg-blue-deep text-white mt-2 p-2 rounded inline-block"
+        @click="toMain"
+      >
         <a href="#maincontent">Skip to main content</a>
       </li>
     </ul>
@@ -12,7 +18,15 @@
 <script lang="ts">
 export default {
   setup() {
-    return {};
+    function toMain() {
+      const inboxHeader = document.getElementById("inbox-header");
+      inboxHeader.tabIndex = 0; //allows you to focus on the inbox header
+      inboxHeader.focus();
+    }
+
+    return {
+      toMain,
+    };
   },
 };
 </script>
