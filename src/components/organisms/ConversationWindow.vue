015dd9a9d51c1552f5e2567678405d09bcb23ef1
shewood <shewood01@gmail.com>
2021-07-06 10:23:56 -0400

    adjusted some heights

--- src/components/organisms/ConversationWindow.vue
+++ src/components/organisms/ConversationWindow.vue
@@ -1,36 +1,36 @@
 <template>
-  <!-- start  test 1 -->
+  <!-- start   -->
   <div
     class="
       w-full
-      h-full
+      h-screen
+      sm:h-full
       flex flex-col flex-1
-      p-4
+      p-2
       sm:p-1
       text-gray-dark
-      sm:overflow-y-auto sm:relative
+      sm:overflow-auto sm:relative
       bg-pink-100
     "
   >
-    <!-- start  test 2 -->
+    <!-- start of scroll window-->
+    <span class="bg-green-200 p-1 h-screen sm:h-full sm:relative">
+      <div
+        class="
+          w-full
+          h-full
+          flex flex-col flex-1
+          sm:overflow-auto
+          p-1
+          text-gray-dark
+          sm:sticky
+          top-0
+          bg-blue-100
+        "
+      >
+        <!-- the header -->
 
-    <!-- start -->
-    <!-- <span class="bg-green-200 p-1  sm:relative"> -->
-
-    <div
-      class="
-        w-full
-        h-full
-        flex flex-col flex-1
-        sm:overflow-auto
-        p-1
-        text-gray-dark
-        bg-blue-100
-      "
-    >
-      <!-- new div here -->
-      <div class="bg-red-400 p-1 flex-1">
-        <div class="lg:sticky top-0 opacity-95 bg-yellow-100 md:opacity-100">
+        <div class="sticky top-0 opacity-95 bg-yellow-100 md:opacity-100">
           <message-header
             backIcon="Back"
             :imageName="chatMessage.senderIcon"
@@ -38,7 +38,8 @@
             :headerText="chatMessage.senderName"
           />
         </div>
-        <div class="flex h-full overflow-auto">
+        <!-- the message window -->
+        <div class="flex h-full sm:overflow-auto">
           <img
             :src="icons[chatMessage.senderIcon]"
             :alt="chatMessage.senderIconAltText"
@@ -69,14 +70,13 @@
             </p>
           </div>
         </div>
+
+        <!-- new div ends here -->
       </div>
-      <!-- new div ends here -->
-    </div>
+    </span>
+    <!-- end of scroll -->
 
-    <!-- </span> -->
-    <!-- end -->
-
-    <!--  end test 2 -->
+    <!--  text input and yes no buttons-->
 
     <div class="sticky bottom-0 w-full">
       <!-- The logic on how the buttonOptions are passed as props will
@@ -89,7 +89,7 @@
     <!-- padding at the bottom of the screen on mobile -->
     <span class="bg-gray-infolt p-2 sm:p-0"></span>
   </div>
-  <!--  end test 1-->
+  <!--  end of component-->
 </template>
 <script>
 import ConversationMessage from "../atoms/ConversationMessage.vue";
