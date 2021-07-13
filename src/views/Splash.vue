2ba957df0724fcb7f0f48f7a3e1d4aef9b2fbfac
Thomas Dacey <DaceyTom2@gmail.com>
2021-07-13 13:55:19 -0400

    designed splash page

--- src/views/Splash.vue
+++ src/views/Splash.vue
@@ -1,6 +1,128 @@
 <template>
   <section>
-    <div class="text-4xl">Splash Page</div>
+    <!-- Background image -->
+    <div class="bg-splash-img h-screen min-w-300px min-h-screen blur"></div>
+    <div
+      class="
+        absolute
+        inset-0
+        -mt-12
+        xl:mb-0
+        flex flex-col
+        justify-center
+        items-center
+      "
+    >
+      <!-- English or French -->
+      <div class="bg-white h-auto min-w-300px w-300px xl:w-500px">
+        <img
+          class="h-auto w-64 container mx-auto pt-6 xl:w-2/3 xl:mx-0 xl:px-6"
+          src="src/assets/sig-blk-en.svg"
+          alt="Symbol of the Government of Canada"
+        />
+        <div class="flex w-max container py-11 mx-auto font-display">
+          <a
+            class="
+              focus:ring-1 focus:ring-black focus:ring-offset-2
+              px-4
+              bg-blue-deep
+              text-white
+              border border-blue-deep
+              active:bg-blue-deep
+              hover:bg-blue-hover
+              text-center text-md
+              w-28
+              xl:w-138px
+              py-3.5
+              mr-6
+              rounded
+              leading-3
+            "
+            id="english-button"
+            data-cy="english-button"
+            role="button"
+            draggable="false"
+            href="/en"
+            >English</a
+          >
+          <a
+            class="
+              focus:ring-1 focus:ring-black focus:ring-offset-2
+              px-4
+              bg-blue-deep
+              text-white
+              border border-blue-deep
+              active:bg-blue-deep
+              hover:bg-blue-hover
+              text-center text-md
+              w-28
+              xl:w-138px
+              py-3.5
+              rounded
+              leading-3
+            "
+            id="french-button"
+            data-cy="french-button"
+            role="button"
+            draggable="false"
+            href="/fr"
+            >Français</a
+          >
+        </div>
+      </div>
+      <!-- Terms & Conditions -->
+      <div
+        class="
+          relative
+          py-8
+          bg-gray-mild
+          text-p
+          h-auto
+          min-w-300px
+          w-300px
+          flex
+          justify-between
+          container
+          p-6
+          xl:w-500px xl:items-center
+        "
+      >
+        <div class="w-28 text-base xl:text-p xl:w-max font-body">
+          <a
+            class="
+              inline-block
+              w-28
+              xl:w-max
+              mr-0
+              hover:underline
+              text-blue-mild
+              splash-link
+              text-lg
+            "
+            href="https://www.canada.ca/en/transparency/terms.html"
+            >Terms &amp; conditions</a
+          >
+          <span class="text-gray-light"> • </span>
+          <a
+            class="
+              inline-block
+              hover:underline
+              text-blue-mild
+              font-body
+              text-lg
+            "
+            href="https://www.canada.ca/fr/transparence/avis.html"
+            lang="fr"
+            >Avis</a
+          >
+        </div>
+        <img
+          class="h-auto w-24 xl:w-28"
+          src="src/assets/wmms-blk.svg"
+          alt="Symbol of the Government of Canada"
+        />
+      </div>
+    </div>
   </section>
 </template>
 
