62e26e3d09aeae53941e569ce779e7afa97954d3
Tisham Islam <45071113+TishamIslam@users.noreply.github.com>
2021-07-27 15:24:36 -0400

    remove overflow-y i accidentally left in the loading state

--- src/components/molecules/Inbox.vue
+++ src/components/molecules/Inbox.vue
@@ -4,7 +4,7 @@
     <div class="px-3 pt-6 pb-3">
       <div class="bg-gray-infolt h-10 w-2/3 animate-pulse"></div>
     </div>
-    <div class="sm:overflow-auto overflow-y space-y-1" role="list">
+    <div class="sm:overflow-auto space-y-1" role="list">
       <!-- Empty inbox items -->
       <inbox-item :inboxItem="{}" />
       <inbox-item :inboxItem="{}" />
