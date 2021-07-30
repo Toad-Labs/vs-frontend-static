d9ec7d1a6c657cdaa30394779107083321fd84cb
Thomas Dacey <DaceyTom2@gmail.com>
2021-07-30 10:59:02 -0400

    added i18n and store to test

--- __tests__/views/Home.spec.js
+++ __tests__/views/Home.spec.js
@@ -1,15 +1,30 @@
-import { shallowMount } from '@vue/test-utils'
+import { mount } from '@vue/test-utils'
 import Home from '../../src/views/Home'
-
-const store = {}
+import { createStore } from 'vuex'
+import { i18n } from "../../i18n";
 
 describe('Test Home.vue view', () => {
 
   it('Check for h1 exists', () => {
-    const wrapper = shallowMount(Home)
+
+    const store = createStore({
+      getters: {
+        "chatMessages/isLoaded": () => true,
+        "emails/isLoaded": () => true,
+      },
+    })
+
+    const wrapper = mount(Home, {
+      global:{
+        provide: {
+          store: store
+        },
+        plugins: [i18n]
+      }
+    })
 
     const h1 = wrapper.find('h1')
     
-    expect(h1.exists().toBe(true))
+    expect(h1.exists()).toBe(true)
   })
 })
