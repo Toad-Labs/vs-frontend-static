907c4cc52187223dc458dd229aa0d057ec77b7cb
Thomas Dacey <DaceyTom2@gmail.com>
2021-07-30 14:32:47 -0400

    tests now linted

--- __tests__/components/atoms/Menu.spec.js
+++ __tests__/components/atoms/Menu.spec.js
@@ -1,6 +1,6 @@
-import { mount } from '@vue/test-utils'
-import Menu from '../../../src/components/atoms/Menu'
+import { mount } from "@vue/test-utils";
+import Menu from "../../../src/components/atoms/Menu";
 
-test('this is the title', () => {
-  console.log(Menu)
-})
+test("this is the title", () => {
+  console.log(Menu);
+});
