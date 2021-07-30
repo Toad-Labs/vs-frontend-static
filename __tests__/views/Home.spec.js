8fd12c8788a6a01842b7e0a2b8eeb507cf186ea5
Thomas Dacey <DaceyTom2@gmail.com>
2021-07-30 12:07:21 -0400

    mocked emails and chat state

--- __tests__/views/Home.spec.js
+++ __tests__/views/Home.spec.js
@@ -2,22 +2,26 @@ import { mount } from '@vue/test-utils'
 import Home from '../../src/views/Home'
 import { createStore } from 'vuex'
 import { i18n } from "../../i18n";
+import chatMessages from "../../src/store/modules/chatMessages"
+jest.mock("../../src/store/modules/chatMessages")
+import emails from "../../src/store/modules/emails"
+jest.mock("../../src/store/modules/emails")
 
 describe('Test Home.vue view', () => {
 
   it('Check for h1 exists', () => {
 
     const store = createStore({
-      getters: {
-        "chatMessages/isLoaded": () => true,
-        "emails/isLoaded": () => true,
-      },
+      modules:{
+        chatMessages,
+        emails,
+      }
     })
-
+    
     const wrapper = mount(Home, {
       global:{
         provide: {
-          store: store
+          store
         },
         plugins: [i18n]
       }
