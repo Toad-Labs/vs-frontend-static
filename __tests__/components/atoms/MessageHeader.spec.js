e959d160df324b7e2a1a7435ab0f32b149140ebe
shewood <shewood01@gmail.com>
2021-08-04 10:15:35 -0400

    added mocks

--- __tests__/components/atoms/MessageHeader.spec.js
+++ __tests__/components/atoms/MessageHeader.spec.js
@@ -1,41 +1,44 @@
 import { shallowMount } from "@vue/test-utils";
 import MessageHeader from "../../../src/components/atoms/MessageHeader";
-import { createStore} from "vuex";
+import { createStore } from "vuex";
 import inbox from "../../../src/store/modules/inbox";
 jest.mock("../../../src/store/modules/inbox");
 
-
-
-
-
 describe("MessageHeader component", () => {
+  let store;
+  beforeAll(() => {
+    store = createStore({
+      modules: {
+        inbox,
+      },
+    });
+  });
+
   it("ensures that the MessageHeader is displayed", () => {
     const wrapper = shallowMount(MessageHeader, {
       props: {
         headerText: "Header Text",
+      },
+      global: {
+        provide: {
+          store,
+        },
       },
     });
     expect(wrapper.text()).toContain("Header Text");
   });
 
   it("ensures that the MessageHeader is displayed", () => {
-    let store = createStore({
-        modules: {
-            inbox,
-          },
-    })
     const wrapper = shallowMount(MessageHeader, {
-     
-
-        
+      global: {
+        provide: {
+          store,
+        },
+      },
     });
     const backButton = wrapper.find("button");
 
     backButton.trigger("click");
-
-  
+    expect(inbox.actions.closeInboxItem).toHaveBeenCalledTimes(1);
   });
-
-
 });
-
