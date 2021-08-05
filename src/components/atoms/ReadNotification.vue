a953f0ae1057ed8097051df650594fe501dd92bc
Tisham Islam <45071113+TishamIslam@users.noreply.github.com>
2021-08-05 10:05:51 -0400

    removed ReadNotification's id values entirely

--- src/components/atoms/ReadNotification.vue
+++ src/components/atoms/ReadNotification.vue
@@ -2,7 +2,6 @@
   <div
     v-if="dayRead"
     class="flex-auto items-baseline h-16 w-16 md:h-20 md:w-20"
-    id="read"
   >
     <p
       class="
@@ -20,7 +19,6 @@
   <div
     v-else
     class="flex place-items-center justify-center h-16 w-16 md:h-20 md:w-20"
-    id="not-read"
   >
     <div class="bg-alert-main rounded-full w-4 h-4" />
   </div>
