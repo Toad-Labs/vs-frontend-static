959152d8d508ddbf53daf8f84a75dd54f1f2ab38
Tisham Islam <45071113+TishamIslam@users.noreply.github.com>
2021-08-05 12:32:02 -0400

    modified tests to test for text instead of elements

--- __tests__/components/atoms/ReadNotification.spec.js
+++ __tests__/components/atoms/ReadNotification.spec.js
@@ -2,28 +2,19 @@ import { mount } from "@vue/test-utils";
 import ReadNotification from "../../../src/components/atoms/ReadNotification";
 
 describe("ReadNotification component", () => {
+  const dayReadProp = "Monday";
   it("ensures that the read notification is displayed", () => {
     const wrapper = mount(ReadNotification, {
       props: {
-        dayRead: "Monday",
+        dayRead: dayReadProp,
       },
     });
-    expect(wrapper.text()).toContain("Monday");
-  });
-  // false by default (not read)
-  it("ensures ReadNotification icon is shown (message has not been read)", () => {
-    const wrapper = mount(ReadNotification);
-    expect(wrapper.find("p").exists()).toBe(false); //unread notification has no text/ <p> tag
+    expect(wrapper.text()).toContain(dayReadProp); //check that prop is displayed
   });
 
-  it("ensures dayRead date is shown (message has been read)", () => {
-    const wrapper = mount(ReadNotification, {
-      data() {
-        return {
-          dayRead: true,
-        };
-      },
-    });
-    expect(wrapper.find("p").exists()).toBe(true); //likewise, the day the message was read should appear here.
+  // false/empty by default (not read)
+  it("ensures ReadNotification icon is shown (message has not been read)", () => {
+    const wrapper = mount(ReadNotification);
+    expect(wrapper.text()).toBe(""); //unread notification has no text
   });
 });
