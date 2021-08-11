be3c2cc310649cd1e08f991462f45bcc9af38ce6
Tisham Islam <45071113+TishamIslam@users.noreply.github.com>
2021-08-11 13:14:58 -0400

    fixed ChatOptionButton tests
    broke due to i18n plugin not included

--- __tests__/components/atoms/ChatOptionButton.spec.js
+++ __tests__/components/atoms/ChatOptionButton.spec.js
@@ -1,21 +1,24 @@
 import { mount } from "@vue/test-utils";
+import { i18n } from "../../../i18n";
 import ChatOptionButton from "../../../src/components/atoms/ChatOptionButton";
 
 describe("ChatOptionButton component", () => {
-  it("ensures text button is displayed", () => {
-    const wrapper = mount(ChatOptionButton, {
-      props: {
-        text: "Yes",
-      },
+  const wrapper = mount(ChatOptionButton, {
+    global: {
+      plugins: [i18n],
+    },
+  });
+
+  it("ensures text button is displayed", async () => {
+    await wrapper.setProps({
+      text: "Yes",
     });
     expect(wrapper.text()).toContain("Yes");
   });
 
-  it("ensures sendFormButton is called on click", () => {
-    const wrapper = mount(ChatOptionButton, {
-      props: {
-        text: "Yes",
-      },
+  it("ensures sendFormButton is called on click", async () => {
+    await wrapper.setProps({
+      text: "Yes",
     });
 
     const quickReplyBtn = wrapper.find("button");
