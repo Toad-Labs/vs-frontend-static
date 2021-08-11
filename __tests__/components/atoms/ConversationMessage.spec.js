3fd55f21b1a568ea92668b8603b4e6f21e0db668
Tisham Islam <45071113+TishamIslam@users.noreply.github.com>
2021-08-11 12:33:57 -0400

    fixed conversation message
    broke due to:
     - using h4 instead of p tags
     - using i18n for hidden text

--- __tests__/components/atoms/ConversationMessage.spec.js
+++ __tests__/components/atoms/ConversationMessage.spec.js
@@ -1,64 +1,59 @@
 import { mount } from "@vue/test-utils";
+import { i18n } from "../../../i18n";
 import ConversationMessage from "../../../src/components/atoms/ConversationMessage";
 
 describe("ConversationMessage component", () => {
-  it("ensures message is displayed", () => {
-    const wrapper = mount(ConversationMessage, {
-      props: {
-        text: "Hello test",
-      },
-    });
+  const wrapper = mount(ConversationMessage, {
+    global: {
+      plugins: [i18n],
+    },
+  });
+
+  it("ensures message is displayed", async () => {
+    await wrapper.setProps({ text: "Hello test" });
     expect(wrapper.text()).toContain("Hello test");
   });
 
-  it("ensures message styling is from bot", () => {
-    const wrapper = mount(ConversationMessage, {
-      propsData: {
-        isUser: false,
-        isLastMessage: true,
-        text: "Hello test",
-      },
+  it("ensures message styling is from bot", async () => {
+    await wrapper.setProps({
+      isUser: false,
+      isLastMessage: true,
+      text: "Hello test",
     });
     expect(wrapper.classes()).toContain("flex");
-    const p = wrapper.find("p");
-    expect(p.classes()).toContain("botMessage");
+    const message = wrapper.find("h4");
+    expect(message.classes()).toContain("botMessage");
   });
 
-  it("ensures message styling is from user", () => {
-    const wrapper = mount(ConversationMessage, {
-      propsData: {
-        isUser: true,
-        isLastMessage: true,
-        text: "Hello test",
-      },
+  it("ensures message styling is from user", async () => {
+    await wrapper.setProps({
+      isUser: true,
+      isLastMessage: true,
+      text: "Hello test",
     });
     expect(wrapper.classes()).not.toContain("flex");
-    const p = wrapper.find("p");
-    expect(p.classes()).toContain("userMessage");
+    const message = wrapper.find("h4");
+    expect(message.classes()).toContain("userMessage");
   });
 
-  it("ensures last message shows img", () => {
-    const wrapper = mount(ConversationMessage, {
-      propsData: {
-        senderIcon: "VA",
-        senderIconAltText: "img alt text",
-        isUser: true,
-        isLastMessage: true,
-        text: "this is my last message",
-      },
+  it("ensures last message shows img", async () => {
+    await wrapper.setProps({
+      senderIcon: "VA",
+      senderIconAltText: "img alt text",
+      isUser: true,
+      isLastMessage: true,
+      text: "this is my last message",
     });
     expect(wrapper.find("img")).toBeTruthy();
   });
 
-  it("ensures not last message does not show img", () => {
-    const wrapper = mount(ConversationMessage, {
-      propsData: {
-        senderIcon: "VA",
-        senderIconAltText: "img alt text",
-        isUser: true,
-        isLastMessage: false,
-        text: "this is my last message",
-      },
+  it("ensures not last message does not show img", async () => {
+    await wrapper.setProps({
+      senderIcon: "VA",
+      senderIconAltText: "img alt text",
+      isUser: true,
+      isLastMessage: false,
+      text: "this is my last message",
     });
     expect(wrapper.find("img").exists()).toBe(false);
   });
