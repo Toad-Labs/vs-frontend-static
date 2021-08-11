ced50cd4171c84fb8b45202c80341c9e59be56e9
Tisham Islam <45071113+TishamIslam@users.noreply.github.com>
2021-08-11 13:23:36 -0400

    fixed ReadNotification test - broke bc of i18n

--- __tests__/components/atoms/ReadNotification.spec.js
+++ __tests__/components/atoms/ReadNotification.spec.js
@@ -1,20 +1,27 @@
 import { mount } from "@vue/test-utils";
+import { i18n } from "../../../i18n";
 import ReadNotification from "../../../src/components/atoms/ReadNotification";
 
 describe("ReadNotification component", () => {
+  const wrapper = mount(ReadNotification, {
+    global: {
+      plugins: [i18n],
+    },
+  });
+
   const dayReadProp = "Monday";
-  it("ensures that the read notification is displayed", () => {
-    const wrapper = mount(ReadNotification, {
-      props: {
-        dayRead: dayReadProp,
-      },
+  it("ensures that the read notification is displayed", async () => {
+    await wrapper.setProps({
+      dayRead: dayReadProp,
     });
     expect(wrapper.text()).toContain(dayReadProp); //check that prop is displayed
   });
 
   // false/empty by default (not read)
-  it("ensures ReadNotification icon is shown (message has not been read)", () => {
-    const wrapper = mount(ReadNotification);
+  it("ensures ReadNotification icon is shown (message has not been read)", async () => {
+    await wrapper.setProps({
+      dayRead: "",
+    });
     expect(wrapper.text()).toBe(""); //unread notification has no text
   });
 });
